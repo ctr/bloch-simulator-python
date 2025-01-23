7949d64b19b5effa542cb267a566ad86e2267f62
Chris Rodgers <639838+ctr@users.noreply.github.com>
2025-01-23 15:45:41 +0000

    Fix some spellings.

--- bloch/rf_seq.py
+++ bloch/rf_seq.py
@@ -2,14 +2,14 @@ import numpy as np
 import scipy as sp
 import scipy.signal as sig
 
-def hard_pulses(b1, flip_angle, seperation, length, count, dt, gamma=26752):
+def hard_pulses(b1, flip_angle, separation, length, count, dt, gamma=26752):
     """
-    Generates a series of hard rf pulses with a specific flip angle seperation and length.
+    Generates a series of hard rf pulses with a specific flip angle separation and length.
     
     Input:
     b1: Amplitude of rf pulse
     flip_angle: Array of flip angle for rf pulse in radians.
-    seperation: Time seperation for different rf puslses in seconds.
+    separation: Time separation for different rf pulses in seconds.
     length: Total time for rf pulse length in seconds. If length is too short, an error will be thrown.
     count: Number of rf pulses in the sequence.
     dt: dt for each index
@@ -20,11 +20,11 @@ def hard_pulses(b1, flip_angle, seperation, length, count, dt, gamma=26752):
     """
     total_units = int(length / dt)
     rf = np.zeros(total_units)
-    seperation_units = int(seperation / dt)
+    separation_units = int(separation / dt)
     for val in range(count):
         pulse_time = flip_angle[val] / (gamma * b1)
         pulse_units = int(pulse_time / dt)
-        start_idx = val * (seperation_units)
+        start_idx = val * (separation_units)
         rf[start_idx:start_idx+pulse_units] = b1 
     return rf
 
